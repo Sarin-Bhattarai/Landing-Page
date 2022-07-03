a2608d8ed76b1c3de756cc227f7f2b621c9abafc
sarin punk <sarin.bhattarai@icp.edu.np>
2022-07-03 20:43:16 +0545

    navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,49 @@
 import React from "react";
-import Logo from "../utils/assets/App.png";
 import "../utils/css/navbar.css";
+import { MenuOutlined } from "@ant-design/icons";
 
 const Navbar = () => {
   return (
-    <div className="navbar-container">
-      <nav className="nav-container">
-        <a href="/">
-          <img className="logo" src={Logo} alt="" />
-        </a>
-        <ul>
-          <li></li>
-        </ul>
-      </nav>
-    </div>
+    <nav className="nav-container">
+      {/* first part */}
+      <a href="/">
+        <h2 className="nav-logo">
+          <span>A</span>pp
+          <span>T</span>echnologies
+        </h2>
+      </a>
+
+      {/* second part */}
+      <ul className="list">
+        <li>
+          <a href="/">HOME</a>
+        </li>
+        <li>
+          <a href="/about">ABOUT</a>
+        </li>
+        <li>
+          <a href="/services">SERVICES</a>
+        </li>
+        <li>
+          <a href="/features">FEATURES</a>
+        </li>
+        <li>
+          <a href="/pricing">PRICING</a>
+        </li>
+        <li>
+          <a href="/clients">CLIENTS</a>
+        </li>
+        <li>
+          <a href="/blog">BLOG</a>
+        </li>
+        <li>
+          <a href="/contact">CONTACT</a>
+        </li>
+      </ul>
+      <button className="btn">
+        <MenuOutlined />
+      </button>
+    </nav>
   );
 };
 
