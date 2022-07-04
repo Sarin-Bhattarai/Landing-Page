1974239b537fb99e7907c0ea787c4fbf7a6ddf78
sarin punk <sarin.bhattarai@icp.edu.np>
2022-07-04 09:14:32 +0545

    feat(navbar-responsive)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../utils/css/navbar.css";
-import { MenuOutlined } from "@ant-design/icons";
+import { MenuOutlined } from "@ant-design/icons"; //changes
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false); //changes
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth); //changes
+
+  //changes
+  const toggleNav = () => {
+    setToggleMenu(!toggleMenu);
+  };
+
+  useEffect(() => {
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", changeWidth);
+
+    return () => {
+      window.removeEventListener("resize", changeWidth);
+    };
+  });
+
   return (
     <nav className="nav-container">
       {/* first part */}
@@ -14,35 +34,39 @@ const Navbar = () => {
       </a>
 
       {/* second part */}
-      <ul className="list">
-        <li>
-          <a href="/">HOME</a>
-        </li>
-        <li>
-          <a href="/about">ABOUT</a>
-        </li>
-        <li>
-          <a href="/services">SERVICES</a>
-        </li>
-        <li>
-          <a href="/features">FEATURES</a>
-        </li>
-        <li>
-          <a href="/pricing">PRICING</a>
-        </li>
-        <li>
-          <a href="/clients">CLIENTS</a>
-        </li>
-        <li>
-          <a href="/blog">BLOG</a>
-        </li>
-        <li>
-          <a href="/contact">CONTACT</a>
-        </li>
-      </ul>
-      <button className="btn">
+
+      {(toggleMenu || screenWidth > 830) && (
+        <ul>
+          <li>
+            <a href="/">HOME</a>
+          </li>
+          <li>
+            <a href="/about">ABOUT</a>
+          </li>
+          <li>
+            <a href="/services">SERVICES</a>
+          </li>
+          <li>
+            <a href="/features">FEATURES</a>
+          </li>
+          <li>
+            <a href="/pricing">PRICING</a>
+          </li>
+          <li>
+            <a href="/clients">CLIENTS</a>
+          </li>
+          <li>
+            <a href="/blog">BLOG</a>
+          </li>
+          <li>
+            <a href="/contact">CONTACT</a>
+          </li>
+        </ul>
+      )}
+      {/* changes */}
+      <span onClick={toggleNav} className="btn">
         <MenuOutlined />
-      </button>
+      </span>
     </nav>
   );
 };
