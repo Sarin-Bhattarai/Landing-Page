323a48d919ec210598789b0e9aee3ce6d10a0008
sarin punk <sarin.bhattarai@icp.edu.np>
2022-07-07 12:40:29 +0545

    feat(service done)

--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -1,10 +1,113 @@
 import React from "react";
+import { Col, Row } from "antd";
+import "../utils/css/feature.css";
+import { FaReact } from "react-icons/fa";
+import { MdMoving } from "react-icons/md";
+import { BsClipboardData } from "react-icons/bs";
+import featureImage from "../utils/assets/featureimg.png";
+import featureSecImage from "../utils/assets/featuresecimg.png";
+import featureThirdImage from "../utils/assets/featurethirdimg.png";
 
 const Features = () => {
   return (
-    <div>
-      <h3>Features</h3>
-    </div>
+    <>
+      <h1 className="feature-header">OUR FEATURES</h1>
+      <Row className="feature-first-row">
+        <Col span={12}>
+          <div>
+            <FaReact className="feature-icon" />
+          </div>
+          <div>
+            <h1 className="feature__header">Marketing Performance</h1>
+            <p className="feature-desc">
+              Itaque earum rerum hic tenetur sapiente delectut reiciendis
+              voluptatibus
+              <br /> perspiciatis unde omnis iste natus error sit maiores alias
+              consequatur
+              <br /> perferendisthat doloribus asperiores repellat.
+              <br />
+              <br />
+              Nam libero tempore cum soluta nobis eligendi optio cumque nihil
+              impedit
+              <br /> minusidquod maxime placeat facere possimus.
+            </p>
+          </div>
+        </Col>
+        <Col span={12}>
+          <div className="border-image">
+            <img
+              className="feature-firstImage"
+              src={featureImage}
+              alt="computer-with-apps"
+            />
+          </div>
+        </Col>
+      </Row>
+
+      <Row className="feature-second-row">
+        <Col span={12}>
+          <div className="border-image-second">
+            <img
+              className="feature-firstImage-second"
+              src={featureSecImage}
+              alt="computer-with-brush"
+            />
+          </div>
+        </Col>
+        <Col span={12}>
+          <div>
+            <MdMoving className="feature-icon-second" />
+          </div>
+          <div>
+            <h1 className="feature__header-second">Strategy Solutions</h1>
+            <p className="feature-desc-second">
+              Itaque earum rerum hic tenetur sapiente delectut reiciendis
+              voluptatibus
+              <br /> perspiciatis unde omnis iste natus error sit maiores alias
+              consequatur
+              <br /> perferendisthat doloribus asperiores repellat.
+              <br />
+              <br />
+              Nam libero tempore cum soluta nobis eligendi optio cumque nihil
+              impedit
+              <br /> minusidquod maxime placeat facere possimus.
+            </p>
+          </div>
+        </Col>
+      </Row>
+
+      <Row className="feature-first-row">
+        <Col span={12}>
+          <div>
+            <BsClipboardData className="feature-icon" />
+          </div>
+          <div>
+            <h1 className="feature__header">Marketing Performance</h1>
+            <p className="feature-desc">
+              Itaque earum rerum hic tenetur sapiente delectut reiciendis
+              voluptatibus
+              <br /> perspiciatis unde omnis iste natus error sit maiores alias
+              consequatur
+              <br /> perferendisthat doloribus asperiores repellat.
+              <br />
+              <br />
+              Nam libero tempore cum soluta nobis eligendi optio cumque nihil
+              impedit
+              <br /> minusidquod maxime placeat facere possimus.
+            </p>
+          </div>
+        </Col>
+        <Col span={12}>
+          <div className="border-image">
+            <img
+              className="feature-firstImage"
+              src={featureThirdImage}
+              alt="computer-with-mic"
+            />
+          </div>
+        </Col>
+      </Row>
+    </>
   );
 };
 
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,10 +1,99 @@
 import React from "react";
+import { Col, Row } from "antd";
+import "../utils/css/services.css";
+import { IoServer, IoColorPalette, IoStatsChart } from "react-icons/io5";
+import { CgIfDesign } from "react-icons/cg";
+import { SiMaterialdesignicons } from "react-icons/si";
+import { RiCheckboxMultipleFill } from "react-icons/ri";
 
 const Services = () => {
   return (
-    <div>
-      <h3>Services</h3>
-    </div>
+    <>
+      <h3 className="service-header">OUR SERVICES</h3>
+      <Row className="services-row">
+        <Col className="container-service" span={8}>
+          <div className="ss-1">
+            <IoServer className="service-icon" />
+          </div>
+          <div className="ss-2">
+            <h1 className="section-headers">Awesome Support</h1>
+            <p className="service-desc">
+              Sed ut perspiciatis unde sit omnise iste natus
+              <br /> voluptatem site accusantium doloremque
+              <br /> laudantium combined with a handful totam.
+            </p>
+          </div>
+        </Col>
+        <Col className="container-service" span={8}>
+          <div>
+            <IoColorPalette className="service-icon" />
+          </div>
+          <div>
+            <h1 className="section-headers">Unlimited Colors</h1>
+            <p className="service-desc">
+              Sed ut perspiciatis unde sit omnise iste natus
+              <br /> voluptatem site accusantium doloremque
+              <br /> laudantium combined with a handful totam.
+            </p>
+          </div>
+        </Col>
+        <Col className="container-service" span={8}>
+          <div>
+            <IoStatsChart className="service-icon" />
+          </div>
+          <div>
+            <h1 className="section-headers">Strategy Solutions</h1>
+            <p className="service-desc">
+              Sed ut perspiciatis unde sit omnise iste natus
+              <br /> voluptatem site accusantium doloremque
+              <br /> laudantium combined with a handful totam.
+            </p>
+          </div>
+        </Col>
+      </Row>
+
+      <Row className="services-second-row">
+        <Col className="container-service" span={8}>
+          <div>
+            <CgIfDesign className="service-icon" />
+          </div>
+          <div>
+            <h1 className="section-headers">Digital Design</h1>
+            <p className="service-desc">
+              Sed ut perspiciatis unde sit omnise iste natus
+              <br /> voluptatem site accusantium doloremque
+              <br /> laudantium combined with a handful totam.
+            </p>
+          </div>
+        </Col>
+        <Col className="container-service" span={8}>
+          <div>
+            <SiMaterialdesignicons className="service-icon" />
+          </div>
+          <div>
+            <h1 className="section-headers">Easy to customize</h1>
+            <p className="service-desc">
+              Sed ut perspiciatis unde sit omnise iste natus
+              <br /> voluptatem site accusantium doloremque
+              <br /> laudantium combined with a handful totam.
+            </p>
+          </div>
+        </Col>
+        <Col className="container-service" span={8}>
+          <div>
+            <RiCheckboxMultipleFill className="service-icon" />
+          </div>
+          <div>
+            <h1 className="section-headers">Truly Multipurpose</h1>
+            <p className="service-desc">
+              Sed ut perspiciatis unde sit omnise iste natus
+              <br /> voluptatem site accusantium doloremque
+              <br /> laudantium combined with a handful totam.
+            </p>
+          </div>
+        </Col>
+      </Row>
+    </>
   );
 };
 
