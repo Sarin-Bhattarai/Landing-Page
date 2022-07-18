07e38a67a3987ebeec1ac9e65865942b739fddb6
sarin punk <sarin.bhattarai@icp.edu.np>
2022-07-18 11:50:59 +0545

    feat(contact page)

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,244 +1,145 @@
 import React from "react";
-import { Col, Row, Button, Form, Input } from "antd";
 import "../utils/css/contact.css";
-import {
-  PhoneOutlined,
-  MailOutlined,
-  HomeOutlined,
-  FacebookOutlined,
-  TwitterOutlined,
-  GooglePlusOutlined,
-  LinkedinOutlined,
-  WhatsAppOutlined,
-} from "@ant-design/icons";
+import { FiMapPin } from "react-icons/fi";
+import { MdPhone, MdOutlineEmail } from "react-icons/md";
+import { Col, Divider, Row } from "antd";
 
 const Contact = () => {
-  const { TextArea } = Input;
   return (
     <>
       <h3 className="contact-header">GET IN TOUCH</h3>
       <br />
-      <div className="wrapper-container">
-        <Row className="contact-row">
-          <Col className="span-right-1" span={12}>
-            <Form
-              name="basic"
-              labelCol={{
-                span: 8,
-              }}
-              wrapperCol={{
-                span: 16,
-              }}
-            >
-              <Form.Item
-                name="Name"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input your name!",
-                  },
-                ]}
-              >
-                <Input
-                  className="input-contact"
-                  placeholder="Name"
-                  style={{
-                    width: "100%",
-                    marginRight: "20px",
-                    height: 45,
-                  }}
-                />
-              </Form.Item>
-
-              <Form.Item
-                name="Email"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input your email!",
-                  },
-                ]}
-              >
-                <Input
-                  className="input-contact"
-                  placeholder="Email"
-                  style={{
-                    width: "100%",
-                    marginRight: "65px",
-                    height: 45,
-                  }}
-                />
-              </Form.Item>
-              <Form.Item
-                labelCol={{
-                  span: 3,
-                }}
-                name="Subject"
-              >
-                <Input
-                  className="subject-to"
-                  placeholder="Subject"
-                  style={{
-                    width: "100%",
-                    height: 45,
-                  }}
-                />
-              </Form.Item>
-
-              <Form.Item
-                labelCol={{
-                  span: 3,
-                }}
-                name="Message"
-              >
-                <TextArea
-                  className="subject-to"
-                  placeholder="Message"
-                  rows={4}
-                />
-              </Form.Item>
-            </Form>
-            <Button type="primary" htmlType="submit">
-              Send Message
-            </Button>
-          </Col>
-          <Col className="span-right-2" span={12}>
-            <div className="contact-first-contact">
-              <PhoneOutlined
-                style={{
-                  fontSize: 25,
-                  color: "black",
-                  opacity: 0.8,
-                }}
-              />
-              <p className="p-p-p">Call: 012-345-6789</p>
-            </div>
-            <div className="contact-first-contact">
-              <MailOutlined
-                style={{
-                  fontSize: 25,
-                  color: "black",
-                  opacity: 0.8,
-                }}
-              />
-
-              <p className="p-p-p"> Email: youremailid@gmail.com</p>
-            </div>
-            <div className="contact-first-contact">
-              <HomeOutlined
-                style={{
-                  fontSize: 25,
-                  color: "black",
-                  opacity: 0.8,
-                }}
-              />
-
-              <p className="p-p-p">
-                Location: 3179 Raccoon Run Seattle, WA 98109
-              </p>
-            </div>
-            <h1>Follow</h1>
-            <div className="icon-footer">
-              <div className="icon-hover">
-                <FacebookOutlined
-                  style={{
-                    fontSize: 18,
-                  }}
-                />
+      <section className="contact">
+        <div className="contact-container">
+          <div className="contactInfo">
+            <div className="box">
+              <div className="contact-icon">
+                <FiMapPin />
               </div>
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <div className="icon-hover">
-                <TwitterOutlined
-                  style={{
-                    fontSize: 18,
-                  }}
-                />
-              </div>
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <div className="icon-hover">
-                <GooglePlusOutlined
-                  style={{
-                    fontSize: 18,
-                  }}
-                />
-              </div>
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <div className="icon-hover">
-                <LinkedinOutlined
-                  style={{
-                    fontSize: 18,
-                  }}
-                />
-              </div>
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <div className="icon-hover">
-                <WhatsAppOutlined
-                  style={{
-                    fontSize: 18,
-                  }}
-                />
+              <div className="contact-text">
+                <h3>Address</h3>
+                <p>
+                  4671 Sugar Camp Road, <br /> Owatonna, Minnesota, <br />
+                  55060
+                </p>
               </div>
             </div>
-          </Col>
-        </Row>
-      </div>
+            <div className="box">
+              <div className="contact-icon">
+                <MdPhone />
+              </div>
+              <div className="contact-text">
+                <h3>Phone</h3>
+                <p>507-475-6094</p>
+              </div>
+            </div>
+            <div className="box">
+              <div className="contact-icon">
+                <MdOutlineEmail />
+              </div>
+              <div className="contact-text">
+                <h3>Email</h3>
+                <p>youremailid@gmail.com</p>
+              </div>
+            </div>
+          </div>
+          <div className="contactForm">
+            <form>
+              <h2>Send Message</h2>
+              <div className="inputBox">
+                <input type="text" name="" required />
+                <span>Full name</span>
+              </div>
+              <div className="inputBox">
+                <input type="text" name="" required />
+                <span>Email</span>
+              </div>
+              <div className="inputBox">
+                <textarea />
+                <span>Type your Message...</span>
+              </div>
+              <div className="inputBox">
+                <input type="submit" name="" value="Send" />
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
 
-      <Row className="contact-row-second">
-        <Col className="res-col-2" span={6}>
-          <h2 className="footer-header-row">ABOUT US</h2>
-          <p className="f-h-r-desc">
-            Sed ut perspiciatis unde omnis iste
-            <br /> natus error sit voluptatem that is
-            <br /> doloremque totam that was.
-          </p>
+      <Row
+        gutter={{
+          xs: 8,
+          sm: 16,
+          md: 24,
+          lg: 32,
+        }}
+      >
+        <Col className="gutter-row" span={6}>
+          <div className="contact-col">
+            <h2>ABOUT US</h2>
+            <p>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              that is doloremque totam that was.
+            </p>
+          </div>
         </Col>
-        <Col className="res-col-2" span={6}>
-          <h2 className="footer-header-row">COMPANY</h2>
-          <ul className="ul-contact">
-            <li>
-              <a href="/#"> Monitoring Grader</a>
-            </li>
-            <li>
-              <a href="/#"> Job Opening</a>
-            </li>
-            <li>
-              <a href="/#"> Customers</a>
-            </li>
-            <li>
-              <a href="/#"> Privacy</a>
-            </li>
-          </ul>
+        <Col className="gutter-row" span={6}>
+          <div className="contact-col">
+            <h2>COMPANY</h2>
+            <ul className="ul-contact">
+              <li>
+                <a href="/#"> Monitoring Grader</a>
+              </li>
+              <li>
+                <a href="/#"> Job Opening</a>
+              </li>
+              <li>
+                <a href="/#"> Customers</a>
+              </li>
+              <li>
+                <a href="/#"> Privacy</a>
+              </li>
+            </ul>
+          </div>
         </Col>
-        <Col className="res-col-2" span={6}>
-          <h2 className="footer-header-row">SUPPORT</h2>
-          <ul className="ul-contact">
-            <li>
-              <a href="/#"> Get Started </a>
-            </li>
-            <li>
-              <a href="/#"> Blog </a>
-            </li>
-            <li>
-              <a href="/#"> Knowledge Base</a>
-            </li>
-          </ul>
+        <Col className="gutter-row" span={6}>
+          <div className="contact-col">
+            <div className="contact-col">
+              <h2>SUPPORT</h2>
+              <ul className="ul-contact">
+                <li>
+                  <a href="/#">Get Started</a>
+                </li>
+                <li>
+                  <a href="/#">Blog</a>
+                </li>
+                <li>
+                  <a href="/#"> Knowledge Base</a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </Col>
-        <Col className="res-col-2" span={6}>
-          <h2 className="footer-header-row">LEGAL</h2>
-          <ul className="ul-contact">
-            <li>
-              <a href="/#"> Terms & Conditions</a>
-            </li>
-            <li>
-              <a href="/#"> Privacy Policy </a>
-            </li>
-            <li>
-              <a href="/#"> Customers</a>
-            </li>
-            <li>
-              <a href="/#"> Pricing</a>
-            </li>
-          </ul>
+        <Col className="gutter-row" span={6}>
+          <div className="contact-col">
+            <div className="contact-col">
+              <h2>LEGAL</h2>
+              <ul className="ul-contact">
+                <li>
+                  <a href="/#">Terms & Conditions</a>
+                </li>
+                <li>
+                  <a href="/#">Privacy Policy</a>
+                </li>
+                <li>
+                  <a href="/#">Customers</a>
+                </li>
+                <li>
+                  <a href="/#">Pricing</a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </Col>
       </Row>
     </>
